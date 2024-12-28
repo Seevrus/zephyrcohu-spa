7481f7d928dc6742cba04d5e00a4a58644b748fe
Seevrus <31958051+Seevrus@users.noreply.github.com>
2024-12-28 14:14:38 +0100

    main component

--- resources/frontend/src/app/app.routes.ts
+++ resources/frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { type Routes } from "@angular/router";
 
-export const routes: Routes = [];
+import { MainComponent } from "./pages/main/main.component";
+
+export const routes: Routes = [
+  {
+    path: "",
+    component: MainComponent,
+  },
+];
