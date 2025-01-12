8e7242e6a154e58bb936f6366f2bc7e216f6bd0a
Seevrus <31958051+Seevrus@users.noreply.github.com>
2025-01-12 23:04:28 +0100

    FE: login component inputs

--- resources/frontend/src/app/pages/login/login.component.ts
+++ resources/frontend/src/app/pages/login/login.component.ts
@@ -1,12 +1,46 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
+import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
+import { MatButton } from "@angular/material/button";
+import { MatFormField } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
 
 @Component({
   selector: "app-login",
   host: {
     class: "app-login",
   },
-  imports: [],
+  imports: [MatButton, MatFormField, MatInputModule, ReactiveFormsModule],
   templateUrl: "./login.component.html",
   styleUrl: "./login.component.scss",
 })
-export class LoginComponent {}
+export class LoginComponent {
+  private readonly formBuilder = inject(FormBuilder);
+  isPasswordVisible = false;
+
+  readonly loginForm = this.formBuilder.group({
+    email: ["", [Validators.required, Validators.email]],
+    password: [
+      "",
+      [
+        Validators.required,
+        Validators.pattern(/^([a-zA-ZíűáéúőóüöÍŰÁÉÚŐÓÜÖ0-9._+#%@-]){8,}$/),
+      ],
+    ],
+  });
+
+  get email() {
+    return this.loginForm.get("email");
+  }
+
+  get password() {
+    return this.loginForm.get("password");
+  }
+
+  onSubmit() {
+    console.log(this.loginForm);
+  }
+
+  togglePassword() {
+    this.isPasswordVisible = !this.isPasswordVisible;
+  }
+}
