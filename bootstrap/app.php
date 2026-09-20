cdade464e51779e3ace17c1a41a224d0421ae4db
Seevrus <31958051+Seevrus@users.noreply.github.com>
2026-09-20 06:48:17 +0200

    ci(deploy): renamed public to public_html

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,7 +18,7 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 use Symfony\Component\HttpKernel\Exception\UnsupportedMediaTypeHttpException;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
@@ -50,3 +50,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $exceptions->render(fn (HttpException $e) => ErrorHandling::internal_Server_error());
     })->create();
+
+$app->usePublicPath($app->basePath('public_html'));
+
+return $app;
